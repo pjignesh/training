1e1c7ba06bf1db287b3ef6cdbc9139a56b71871c
pjignesh <pjignesh@trellissoft.ai>
2024-03-28 15:25:26 +0530

    Initial

---
+++ MyMath/calculator.py
@@ -0,0 +1,17 @@
+info = "Simple Calculator"
+
+def sqr(n):
+    return n * n
+
+class Calc():
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
---
+++ my_code.py
@@ -0,0 +1,13 @@
+# from MyMath.calculator import *
+
+# print(info)
+# print(sqr(3))
+# print(Calc().sub(8,2))
+
+# import os
+# os.remove("demofile2.txt")
+
+#open and read the file after the appending:
+f = open("demo_file.txt", "r")
+print(f.read())
+
